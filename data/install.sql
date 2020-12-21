f7794d451372bfe1602f99f6809d272a74c7910d
liuwang <616458279@qq.com>
2020-12-21 11:12:17 +0800

    修改项目名称-规范化

--- data/install.sql
+++ data/install.sql
@@ -1,8 +1,10 @@
+
 CREATE TABLE `__PREFIX__admin_email` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `email` varchar(60) DEFAULT '' COMMENT '描述',
-  `create_time` datetime NOT NULL DEFAULT '2020-01-01 00:00:00' COMMENT '添加时间',
-  `update_time` datetime NOT NULL DEFAULT '2020-01-01 00:00:00' COMMENT '更新时间',
-  `enable` tinyint(4) NOT NULL DEFAULT '1' COMMENT '1:开启/0:禁用',
+  `email` varchar(60) NOT NULL DEFAULT '' COMMENT '邮箱',
+  `enable` tinyint(4) unsigned NOT NULL DEFAULT '1' COMMENT '1:开启/0:禁用',
+  `create_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '添加时间',
+  `update_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `remark` varchar(20) NOT NULL COMMENT '备注',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='邮箱记录表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='邮箱记录表';
