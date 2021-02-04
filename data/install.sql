8d5d0a2f90e9e8aa1d02169c8b908529f7c0c6b0
liuwang <616458279@qq.com>
2021-02-04 09:49:28 +0800

    更新安装sql

--- data/install.sql
+++ data/install.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS `__PREFIX__admin_email` (
   `email` varchar(60) NOT NULL DEFAULT '' COMMENT '邮箱地址',
   `name` varchar(60) NOT NULL DEFAULT '' COMMENT '邮箱昵称',
   `enable` tinyint(4) unsigned NOT NULL DEFAULT '1' COMMENT '1:开启/0:禁用',
-  `create_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
-  `update_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `create_time` datetime NOT NULL DEFAULT '2020-01-01 00:00:00' COMMENT '添加时间',
+  `update_time` datetime NOT NULL DEFAULT '2020-01-01 00:00:00' COMMENT '更新时间',
   `remark` varchar(20) NOT NULL DEFAULT '' COMMENT '备注',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='邮箱记录表';
